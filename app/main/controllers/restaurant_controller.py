7e9360853b824b27beeae2650d290a1e45039aa2
Yashwith D Alva <helos6302@gmail.com>
2023-11-07 01:44:15 +0530

    Added routing functionality to restaurant

--- app/main/controllers/restaurant_controller.py
+++ app/main/controllers/restaurant_controller.py
@@ -4,11 +4,13 @@ A simplified interface used for routing
 
 from flask import jsonify
 from services.restaurant_service import RestaurantService
+import main.models.restaurant
 
 class RestaurantController:
     def __init__(self):
         self.restaurantService_ = RestaurantService()
 
+    # Create a new restaurant
     def create_restaurant(self, request):
         data = request.get_json()
         resName = data.get('resName')
@@ -23,6 +25,10 @@ class RestaurantController:
         restaurant_id = self.restaurantService_.create_restaurant(resName, about, qr, address, location_link)
         return jsonify({'message': 'Restaurant created Successfully', 'restaurant id': restaurant_id}), 201
     
+    # Return all restaurants
+    def get_all_restaurants(self):
+        restaurants = self.restaurantService_.get_all_restaurants()
+        restaurants_list = [{'rid':row.restaurant_id, 'resName': row.restaurant_name, 'about': row.about, 'qr': row.qr, 'address':row.address, 'locationLink': row.locationLink} for row in restaurants]
+        return jsonify(restaurants_list), 200
     
     
-        
--- app/main/routes.py
+++ app/main/routes.py
@@ -6,13 +6,11 @@ Date: 6-11-2023
 """
 
 from . import main_blueprint
-
-
-# import controllers
-# from app.main.controllers
+from main.controllers import restaurant_controller
 
 # create an object out of the controller
+restaurantController = restaurant_controller()
 
-#@main_blueprint.route('/users', methods=['GET'])
-#def get_users():
-    #return user_controller.get_users()
+@main_blueprint.route('/resturant', methods=['GET'])
+def get_users():
+    return restaurantController.get_all_restaurants()
